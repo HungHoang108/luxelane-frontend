6e6d56cd9a82ca8f7ec3890e56e93772dc614e58
hung hoang <hung.xamk@gmail.com>
2022-12-27 22:37:59 +0200

    added search page and searching feature

--- src/components/routes/searchResult/searchResult.route.tsx
+++ src/components/routes/searchResult/searchResult.route.tsx
@@ -1,31 +1,34 @@
 import { useAppSelector } from "../../../hooks/reduxHook";
 
-
 import Button from "../../button/button.component";
 
-
 const SearchResult = () => {
-  const products = useAppSelector(state => state.productReducer);
-  const searchQuery = useAppSelector(state => state.SearchTagReducer)
+  const products = useAppSelector((state) => state.productReducer);
+  const searchQuery = useAppSelector((state) => state.SearchTagReducer);
+  const searchQueryLowerCase = searchQuery.toLowerCase();
 
   return (
     <>
       <h1>Search Result</h1>
       <div className="products">
-        {products.map(product => (
-          <div key={product.id} className="products-card">
-            <img src={product.images[0]} alt="" />
-            <h4>{product.title}</h4>
-            <h4>{product.price} $</h4>
-            <Button
-              id={product.id}
-              itemName={product.title}
-              image={product.images[0]}
-              price={product.price}
-              amount={1}
-            />
-          </div>
-        ))}
+        {products.map((product) => {
+          if (product.title.toLowerCase().includes(searchQueryLowerCase)) {
+            return (
+              <div key={product.id} className="products-card">
+                <img src={product.images[0]} />
+                <h4>{product.title}</h4>
+                <h4>{product.price} $</h4>
+                <Button
+                  id={product.id}
+                  itemName={product.title}
+                  image={product.images[0]}
+                  price={product.price}
+                  amount={1}
+                />
+              </div>
+            );
+          }
+        })}
       </div>
     </>
   );
