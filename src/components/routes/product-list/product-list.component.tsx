937ce6a4eb34d8ebbf5054140587366837128600
hung hoang <hung.xamk@gmail.com>
2022-12-27 22:58:27 +0200

    adding sort options

--- src/components/routes/product-list/product-list.component.tsx
+++ src/components/routes/product-list/product-list.component.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { ChangeEvent, useState } from "react";
 import ProductCard from "../../product-card/product-card.component";
 
 const ProductList = () => {
+  const [sortOption, setSortOption] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSortOption(e.target.value);
+  };
   return (
     <div>
+      <div>
+        <select onChange={handleChange} id="sort">
+          <option>Sort products</option>
+          <option value="category">Category</option>
+          <option value="price-down">From highest price</option>
+          <option value="price-up">From lowest price</option>
+        </select>
+      </div>
       <ProductCard title="All Products" productsDisplayed={8} />
     </div>
   );
