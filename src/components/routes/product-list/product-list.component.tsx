dd7564dbfe664e56da5bda807931190f4251b2a8
hung hoang <hung.xamk@gmail.com>
2022-12-28 16:59:33 +0200

    filtering products based on category

--- src/components/routes/product-list/product-list.component.tsx
+++ src/components/routes/product-list/product-list.component.tsx
@@ -2,23 +2,36 @@ import { ChangeEvent, useState, useEffect } from "react";
 import ProductCard from "../../product-card/product-card.component";
 import { useAppDispatch } from "../../../hooks/reduxHook";
 import { sortByPrice } from "../../../redux/products-reducer";
+import { useAppSelector } from "../../../hooks/reduxHook";
 
 const ProductList = () => {
   const dispatch = useAppDispatch();
+  const products = useAppSelector((state) => state.productReducer);
 
   const [sortOption, setSortOption] = useState("");
+
   useEffect(() => {
     dispatch(sortByPrice(sortOption));
   }, [sortOption]);
+
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value);
     setSortOption(e.target.value);
   };
+
   return (
     <div>
       <div>
         <select onChange={handleChange} id="sort">
           <option>Sort products</option>
-          <option value="category">Category</option>
+
+          <optgroup label="Category">
+            <option value="Furniture">Furniture</option>
+            <option value="Clothes">Clothes</option>
+            <option value="Electronics">Electronics</option>
+            <option value="Shoes">Shoes</option>
+            <option value="Others">Others</option>
+          </optgroup>
           <option value="price-down">From highest price</option>
           <option value="price-up">From lowest price</option>
         </select>
